d4e704de7761f8bd086d211b5b53e1367628ebdf
ElHefnawy1290 <amr223489@gmail.com>
2025-07-22 20:52:46 +0300

    final

--- Assignment1/Task2/Program.cs
+++ Assignment1/Task2/Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using System.Diagnostics;
+using System.Threading;
 
-Console.WriteLine("Hello, World!");
+
+namespace Task2
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Please enter a valid input.");
+                return;
+            }
+
+            long n = long.Parse(input);
+
+            // Validate input
+            if (n <= 0)
+            {
+                Console.WriteLine("Please enter a positive integer.");
+                return;
+            }
+
+            long sum = 0;
+
+            // Formula approach
+            Stopwatch stopwatch1 = Stopwatch.StartNew();
+
+            sum = n * (n + 1) / 2;
+
+            stopwatch1.Stop();
+
+            double elapsedMilliseconds1 = stopwatch1.ElapsedMilliseconds;
+
+            // Loop approach
+            sum = 0;
+            Stopwatch stopwatch2 = Stopwatch.StartNew();
+
+            for (long i = 1; i <= n; i++)
+                sum += i;
+
+            stopwatch2.Stop();
+
+            double elapsedMilliseconds2 = stopwatch2.ElapsedMilliseconds;
+
+            // Output results
+            Console.WriteLine($"Execution Time Formula (ms): {elapsedMilliseconds1}");
+            Console.WriteLine($"Execution Time (ms) for loop: {elapsedMilliseconds2}");
+        }
+    }
+}
--- Assignment1/Task3/Program.cs
+++ Assignment1/Task3/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using System.Diagnostics;
+using System.Threading;
 
-Console.WriteLine("Hello, World!");
+
+namespace Task3
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            long number = long.Parse(Console.ReadLine());
+            for (long i = 1; i <= number; i++)
+            {
+                sb.Append(i);
+                if (i < number) sb.Append(", ");
+            }
+            Console.WriteLine(sb.ToString());
+
+            // The complexity of this code Amortized is O(n)
+            // O(n) for the for loop and O(1) for appending in the stringBuilder
+            // The space complexity is O(n) for storing the string in the StringBuilder
+            // Also the stringBuilder may need O(n) time to resize and handle more characters
+            // but it is amortized, so we consider it O(1) for each append
+
+            // It's an array of characters that is used to build strings efficiently
+            // It is mutable, meaning you can change its content without creating a new object
+            // in contrast of normal strings which are immutable
+            // And it's more efficient in appending than normal strings as it appends in O(1)
+        }
+    }
+}
